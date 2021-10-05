9ead053396acc6b359531a6471b59c0718d0748c
Sean Ringrose <seanringrose12@gmail.com>
2021-10-05 15:46:22 -0500

    Update code to actually do things
    Built out findChannel function to find the channel.
    Added logic to determine if findChannel successful, and if not to insert current channel and the character object (as init_table). If successful, the function will pull the init_table, push the char and roll to the init_table, and then update the record in the db collection.

--- initbot.js
+++ initbot.js
@@ -23,26 +23,39 @@ function dbCreateSchema() {// Creates the base Records Schema format
 
 // Add a character to the initiative list
 function addChar(currchan, name, roll) {
-    findChannel(currchan);
     
+    // sets char object to name and roll
     let char = {
         'name': name,
         'roll': roll
     }
     
-    db.collection('records').insertOne({
-        channel: currchan,
-        init_table: char
-    });
-
-    console.log(db.collection('records').findOne({channel: currchan}))
+    // if current channel not found in db collection, then create record for channel in db collection
+    if (!findChannel(currchan)) {
+        db.collection('records').insertOne({
+            channel: currchan,
+            init_table: char
+        })        
+    } else { // if current channel found in db collection, then pull the record, set local init to record's init_table, push char object to init_table, and then update the record in the db collection
+        let record = db.collection('records').findOne({channel: currchan})
+        let init = record.init_table
+        init.push(char)
+        db.collection('records').updateOne({channel: currchan},
+            { 
+                $set: {init_table: init}})
+    } 
 }
 
 // Search record for currchan
 function findChannel(currchan){
-    db.collection('records').findOne({channel: currchan})
-    console.log(db.collection('records').findOne({channel: currchan}), currchan);       
+    if (!db.collection('records').find({channel: currchan})) {
+        return false;
+    }
+    else { return true;
+    }
 }
 
-addChar(1, 'xzee', '3.2.1');
+addChar(1234, 'xzee', '3.2.1');
+addChar(1234, 'skye', '3.0');
+addChar(1234, 'zakar', '3.1.1');
 
