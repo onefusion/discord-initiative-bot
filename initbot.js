dcc7d6f7f7dd2784061a09abcdfbca77c0849a30
Sean Ringrose <seanringrose12@gmail.com>
2021-10-05 19:23:39 -0500

    Update initbot.js

--- initbot.js
+++ initbot.js
@@ -19,10 +19,8 @@ function dbCreateSchema() {// Creates the base Records Schema format
     recordTable=mongoose.model('records', recordSchema);
 }
 
-
-
 // Add a character to the initiative list
-function addChar(currchan, name, roll) {
+async function addChar(currchan, name, roll) {
     
     // sets char object to name and roll
     let char = {
@@ -30,36 +28,41 @@ function addChar(currchan, name, roll) {
         'roll': roll
     }
     
+    await db.collection('records').findOneAndUpdate({channel: currchan},
+        {
+            $get: {init_table}
+           },
+        
+        {           
+            $set: {channel: currchan, init_table: roll}}
+        )
     // if current channel not found in db collection, then create record for channel in db collection
     if (!findChannel(currchan)) {
-        db.collection('records').insertOne({
+        await db.collection('records').insertOne({
             channel: currchan,
             init_table: char
         })        
     } else { // current channel is found in db collection -- pull record
-        let record = db.collection('records').findOne({channel: currchan})
-        
+        let record = await db.collection('records').findOne({channel: currchan})
+        console.log(record)
         // set local init to record's init_table and push char object to local init
-        let init = record.init_table
-        init.push(char)
+          record.init_table.push(char)
         
         // and then update the record's init_table in the db collection
-        db.collection('records').updateOne({channel: currchan},
+        await db.collection('records').findOneAndUpdate({channel: currchan},
             { 
-                $set: {init_table: init}})
+                $set: {init_table: init}
+            }
+        )
     } 
 }
 
 // Search record for currchan
-function findChannel(currchan){
-    if (!db.collection('records').find({channel: currchan})) {
-        return false;
-    }
-    else { return true;
-    }
-}
 
-addChar(1234, 'xzee', '3.2.1');
-addChar(1234, 'skye', '3.0');
-addChar(1234, 'zakar', '3.1.1');
+addChar(1001, 'xzee', '3.2.1');
+addChar(1001, 'skye', '3.0');
+addChar(1001, 'zakar', '3.1.1');
+addChar(1002, 'xzee', '1.2.3');
+addChar(1001, 'jannik', '4.0.1');
+addChar(1002, 'skye', '1.1.2');
 
