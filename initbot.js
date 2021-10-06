f8261d4209a65e4fb1c5dc07271b9b33aeb5efcf
Sean Ringrose <seanringrose12@gmail.com>
2021-10-06 18:55:11 -0500

    Update code to refactor for async / await functions properly

--- initbot.js
+++ initbot.js
@@ -11,7 +11,7 @@ function dbCreateSchema() {// Creates the base Records Schema format
             type: String,
             required: true
         },
-        init_table: {
+        initiative_table: {
             type: Array
         }
     })
@@ -29,72 +29,61 @@ async function addChar(currchan, name, roll) {
         'name': name,
         'roll': roll
     }
+
+    let initiative = char
     
-    /**await db.collection('records').findOneAndUpdate({channel: currchan},
-        {
-            $get: {init_table}
-           },
-        
-        {           
-            $set: {channel: currchan, init_table: roll}}
-        )
-    **/
     // if current channel not found in db collection, then create record for channel in db collection
-    
-    /*      ------------------------------------------------------------------------------------------------------------------
+    /*    ------------------------------------------------------------------------------------------------------------------
             findChannel
 
             Would it make sense to move this bit to a bot or channel initialization? 
             We'd want to be sure the channel record exists no matter which command is first. 
-
-
-            Also, is findChannel a discord function or one you wrote? I don't see it in the code here. 
           ------------------------------------------------------------------------------------------------------------------*/
-    if (!findChannel(currchan)) {
-        await db.collection('records').insertOne({
-            channel: currchan,
-            init_table: char
-        }).exec();        
-    /*      ------------------------------------------------------------------------------------------------------------------
-            If we move the above bit to a separate function for initialization, we don't need this wrapped in an else
+    if (! await findChannel(currchan)) {
+        await db.collection('records').insertOne({channel: currchan,init_table: initiative})      
+    /*    ------------------------------------------------------------------------------------------------------------------
+            If we move the above bit to a separate function for initiativeialization, we don't need this wrapped in an else
           ------------------------------------------------------------------------------------------------------------------*/
           
-    } else { // current channel is found in db collection -- pull record and set init to init_table
-        let init = await db.collection('records').findOne({channel: currchan}, "init_table").exec();
-        console.log(init)
+    } else { // current channel is found in db collection -- pull record and set initiative to initiative_table
+        let initiative = await db.collection('records').find({channel: currchan}).project({init_table:1, _id: 0})
+        console.log(initiative)
 /*      ------------------------------------------------------------------------------------------------------------------
             Also, the next step before just pushing the character should be to check the existing table to see if it has 
                 the character already. If it already has the character, do we give a message to say that character is already 
                 in the list, and the update command should be used? 
-          ------------------------------------------------------------------------------------------------------------------*/
+        ------------------------------------------------------------------------------------------------------------------*/
 
-        // push char object to local init
-          init.push(char)
+        // push char object to local initiative
+        //initiative.push(char)
+        //console.log(initiative)
         
         // and then update the record's init_table in the db collection
         await db.collection('records').findOneAndUpdate({channel: currchan},
-            { 
-                $set: {init_table: init}
-            }
-        ).exec();
+            {
+                $set: {init_table: initiative}
+            })
     } 
 }
 
 // Search record for currchan
 async function findChannel(currchan) {
 
-    if (await db.collection('records').findOne({channel: currchan}).exec() === null) {
+    if (await db.collection('records').findOne({channel: currchan}) === null) {
+        console.log('channel not found')
         return false;
     }
     else {
+        console.log('channel found')
         return true;
     }
 }
 
+dbCreateSchema();
 addChar(1001, 'xzee', '3.2.1');
 addChar(1001, 'skye', '3.0');
-addChar(1001, 'zakar', '3.1.1');
+//addChar(1001, 'zakar', '3.1.1');
 addChar(1002, 'xzee', '1.2.3');
-addChar(1001, 'jannik', '4.0.1');
-addChar(1002, 'skye', '1.1.2');
+//addChar(1001, 'jannik', '4.0.1');
+//addChar(1002, 'skye', '1.1.2');
 
