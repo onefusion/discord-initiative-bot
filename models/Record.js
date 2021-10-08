b6d85110c799132736a98b1e2d8271dff8733103
Sean Ringrose <seanringrose12@gmail.com>
2021-10-07 20:59:16 -0500

    update record.js and fix unique name thing

--- models/Record.js
+++ models/Record.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 let recordSchema = new Schema({
-    channel: { type: String, unique: true, required: true },
-    initiative: [{ name: String, unique: true, roll: String }]
+    channel: { type: String, required: true, index: true, unique: true },
+    initiative: [{ name: String , roll: String }]
 })
 
 module.exports = mongoose.model('Record', recordSchema)
