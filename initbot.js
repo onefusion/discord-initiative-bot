367f419b63176f4d6528652876fa5dc7cb49bf24
Sean Ringrose <seanringrose12@gmail.com>
2021-10-07 19:48:07 -0500

    update initbot.js

--- initbot.js
+++ initbot.js
@@ -18,7 +18,7 @@ async function addChar(currchan, name, roll) {
             Would it make sense to move this bit to a bot or channel initialization? 
             We'd want to be sure the channel record exists no matter which command is first. 
           ------------------------------------------------------------------------------------------------------------------*/
-    if (findChannel(currchan) === null) {
+    if (findChannel(currchan) == '1') {
         let newRecord = new Record({
             channel: currchan,
             initiative: [{name: name, roll: roll}]
@@ -78,7 +78,7 @@ async function findChannel(currchan) {
     if (record === null) {
         debugmsg('channel not found (if record is null)')
         debugmsg('record is undefined')
-        return record
+        return '1'
     }
     else {
         if (record.channel == currchan) {
