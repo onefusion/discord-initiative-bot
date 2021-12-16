be37f136911f55046acbd942dafd2f46b1067027
Sean Ringrose <seanringrose12@gmail.com>
2021-12-16 11:14:01 -0600

    Refactored initbot

--- initbot.js
+++ initbot.js
@@ -1,87 +1,90 @@
-const Discord = require('discord.js');
+// const Discord = require('discord.js');
 const config = require('./config.json');
 let mongoose = require('mongoose');
 const db = require('./database.js');
 const Record = require('./models/Record');
+const { findOne } = require('./models/Record');
 
 //const client = new Discord.Client();
 
 let debug = true; //Set if debug mode is active or inactive
+global.record = {};
+
+/* @TODO Add function for updating initiative table instead of doing it all in addToInitiative function
+*/
 
 // Add a character to the initiative list
-async function addChar(currchan, name, roll) {
+async function addToInitiative(currchan, name, roll) {
+    
+    // run findChannel function passing currchan variable
+    let foundChan = await findChannel(currchan)
+    debugmsg('addToInitiative: foundChan is ' + foundChan)
     
     // if current channel not found in db collection, then create record for channel in db collection
-    
-    let foundChan = await findChannel(currchan)
-          debugmsg('addChar: foundChan is ' + foundChan)
     if (!foundChan) {
-        let newRecord = await new Record({
-            channel: currchan,
-            initiative: {name: name, roll: roll},
-        })
-        debugmsg('addChar: findChannel preparing new record')
-
-        await newRecord.save(function (err, doc){
-            if (err) console.error('newRecord.save err: ' + err)
-                debugmsg('addChar err: ' + doc)
-        }) 
-        debugmsg('newRecord save completed: ' + newRecord)             
-    } else { // current channel is found in db collection -- pull record and set initiative to initiative_table
-        let record = await Record.findOne({channel: currchan}).exec()
-        debugmsg('addChar: addChar if record is in db statement')
-        //debugmsg('addChar: ' + await Record.findOne({channel: currchan}))
-        debugmsg('addChar: init to add: name ' + name + ', roll ' + roll)
         
-        // convert record's initiative object to key's array
-        const keys = Object.keys(record.initiative)
-        debugmsg('Keys array: '+keys)
+        // create new record for channel in db
+        debugmsg('addToInitiative: call addNewRecord')
+        record = await addNewRecord(currchan, name, roll)
 
-        keys.forEach((key,index) => {
-            debugmsg(`${key}: ${record.initiative[key]}`)
+    } 
+    
+    // current channel is found in db collection -- pull record and set rolls to record's initiative
+    else { 
+            
+        // check for record matching currchan and name
+        record = await findChar(currchan, name, roll)
+            
+        // if record was not found matching currchan and name, then create a new record
+        if (!record) {
+                
+            // create new record
+            record = await addNewRecord(currchan, name, roll)
+            debugmsg('addToInitiative, create new record: ' + record)
 
-            if (record.initiative[key] == name) {
-                    // update character's roll
-                    record.initiative[key] = roll
-                    debugmsg('addChar; record initiative: : addChar if record is found in db, and record.initiative.name is same as name')
-                    debugmsg('addChar; record initiative added: name: ' + name + ', roll: ' + roll)
-                    debugmsg('addChar; record initiative updated: ' + record.initiative)
-                }
-            else {  // sets char object to name and roll
-                let char = {
-                    'name': name,
-                    'roll': roll,
-                }
+        }
 
-                // push char to record
-                record.initiative.push(char)
-                debugmsg('addChar; record initiative: ' + record.initiative)
-                debugmsg('addChar; record initiative: adding char ' + char)
-                //debugmsg('addChar; record initiative: ' + Record.findOne({channel: currchan}).exec())
-            } 
+        // record found matching currchan and name, so set roll in record to roll
+        else { 
+                
+            // set roll in record to roll
+            record = await setRoll(record, roll)
+            debugmsg('addToInitiative: setRoll: ' + record)
 
-            // and then save the updated record
-            record.save(function (err, doc){
-                if (err) console.error('record.save: ' ,err)
-                debugmsg('addChar; record save doc: ' ,doc)
-            })
-    })   
-    }    
-}
+        }
+    }
+    
+    // and then save the record
+    debugmsg('addToInitiative: save record')
+    let save = await saveRecord(record)
+    
+}   
+    
 
 // Search record for currchan
 async function findChannel(currchan) {
     
+    // set record to result of findOne matching channel to currchan
     let record = await Record.findOne({channel: currchan}).exec()
-    debugmsg('findChannel; record.findOne: ' + record)
+    debugmsg('findChannel: searching for channel ' + currchan)
+    debugmsg('findChannel: record.findOne: ' + record)
     
+    // in mongoose or mongodb, if the findOne does not find a match it will return null, so is record null?
     if (record === null) {
 
         debugmsg('findChannel: channel not found (record is null)')
+        
+        // pass null to next function so it can know record does not exist for currchan value
         return null;
         
     }
+    
+    // if record found, then record is not null
     else {
+        
+        // record is not null, but is the found record's channel equal to currchan?
+        // return the record found either way
+        // note: this if/else is not necessary, but we wanted to include for logging purposes in case it bugs unexpectedly
         if (record.channel == currchan) {
             debugmsg('findChannel: channel found and is currchan')
             debugmsg('findChannel: channel '+record.channel)
@@ -95,21 +98,72 @@ async function findChannel(currchan) {
     }
 }
 
+// This function searches for channel and character name
+async function findChar(currchan, name) {
+
+    let record = await findOne({channel: currchan}, {name: name})
+    debugmsg('findChar: ' + record)
+    return record
+
+}
+
+// This function creates a new Record
+async function addNewRecord(currchan, name, roll){
+    
+    // create new record from recordSchema
+    let record = await new Record({
+        channel: currchan,
+        name: name, 
+        roll: roll,
+    })
+    
+    debugmsg('addNewRecord: ' + record)
+    return record
+
+}
+
+// This function saves a record in the database
+async function saveRecord(record) {
+    
+    await record.save(function (err, doc){
+        if (err) console.error('record.save: ' ,err)
+        debugmsg('saveRecord: record save doc: ' ,doc)
+    })
+
+}
+
+// This function sets the roll of a record in the database
+async function setRoll(record, roll) {
+
+    // set record.roll to roll
+    debugmsg('setRoll: record.roll is ' + record.roll)
+    record.roll = roll
+    
+    // return updated record
+    debugmsg('setRoll: record updated to ' + record)
+    return record
+
+}
+
 //This function checks if debug mode is enabled (true)
 function debugmsg(msg) {
+
     if (debug) {
         console.log(msg)
     }
+
 }
 
 //This function can toggle the debug mode variable. Ternary operator to simplify the statement. 
 function toggleDebug() {
+
     debug = debug ? false : true;
+
 }
 
-addChar(10, 'Xzee', '3.2.1');
-addChar(10, 'Skye', '3.0');
-//addChar(10, 'Zakar', '3.1.1');
-//addChar(11, 'Xzee', '1.2.3');
-//addChar(11, 'Jannik', '4.0.1');
-//addChar(11, 'Skye', '1.1.2');
+addToInitiative(10, 'Xzee', '2.1.0');
+addToInitiative(10, 'Skye', '2.0');
+addToInitiative(10, 'Zakar', '2.1.1');
+addToInitiative(11, 'Xzee', '1.1');
+addToInitiative(11, 'Jannik', '1.0.1');
+addToInitiative(11, 'Skye', '1.1.1');
