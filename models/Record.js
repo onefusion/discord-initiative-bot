58355e06ffba4279657d4e9635d07798a2255b14
Sean Ringrose <seanringrose12@gmail.com>
2021-12-16 11:13:36 -0600

    Refactored initbot code to be easier to read and more adaptable later

--- models/Record.js
+++ models/Record.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
+
 const recordSchema = new mongoose.Schema({
-    channel: {  type: String, 
-                required: true, 
-                index: true },
-    initiative: {
-        name: { type: String, lowercase: true }, 
-        roll: String},
+    channel: String,
+    name: { type: String, lowercase: true },
+    roll: String,
 })
 
 module.exports = mongoose.model('Record', recordSchema)
